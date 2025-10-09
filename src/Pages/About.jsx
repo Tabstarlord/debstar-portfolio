dc94c86b54351ad89eb4ced8cf575dd10b7d5559
Tabstarlord <henrystarizukanne@gmail.com>
2025-10-09 05:05:24 +0100

    initial commit

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -6,6 +6,8 @@ import Code from '../assets/coding.png'
 import Tailwind from '../assets/tailwind.png'
 import Experience from '../assets/experience.png'
 import Education from '../assets/education.png'
+import Bulb from '../assets/bulb.png'
+import Chat from '../assets/message.png'
 
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
@@ -18,12 +20,12 @@ export default function About() {
         <h1 className='text-xl text-center font-black mt-5 mb-2 mx-2 md:text-2xl lg:text-3xl'>Crafting Seamless Web Experience</h1>
         <p className='mx-2 text-center mb-5'>A dedicated Frontend Developer passionte about building intuitive,performant, and visually appealing web applications. I thrive on bringing ideas to life with elegant code and thoughtful design.</p>
 
-        <div className='bg-gray-600 space-y-4 md:flex md:gap-5'>
-          <div className='bg-gray-500 rounded-md pt-3 space-y-2 md:w-full md:m-3 md:self-start md:rounded-lg'>
+        <div className='bg-gray-800 space-y-4 md:flex md:gap-5'>
+          <div className='bg-gray-700 rounded-md pt-3 space-y-2 md:w-full md:m-3 md:self-start md:rounded-lg'>
             <h5 className='text-2xl font-black mx-2'>Technical Skills</h5>
             <ul className='space-y-6 mx-2 pb-5'>
               <li className='flex justify-between mx-2 items-center'>
-                <img src={React} alt='Reactjs' className='w-5 h-5' />
+                <img src={React} alt='Reactjs' className='w-8 h-8' />
                 <p className='text-md font-medium'>React.js</p>
                 <input type='range' value={8} min={0} max={10} className='w-20' />
                 <div className='space-y-2'>
@@ -33,7 +35,7 @@ export default function About() {
               </li>
 
               <li className='flex justify-between mx-2 items-center'>
-                <img src={Javascript} alt='Javascript' className='w-5 h-5' />
+                <img src={Javascript} alt='Javascript' className='w-8 h-8' />
                 <p className='text-md font-medium'>Javascript</p>
                 <input type='range' value={6} min={0} max={10} className='w-20' />
                 <div className='space-y-2'>
@@ -43,7 +45,7 @@ export default function About() {
               </li>
 
               <li className='flex justify-between mx-2 items-center'>
-                <img src={Html} alt='Html' className='w-5 h-5' />
+                <img src={Html} alt='Html' className='w-8 h-8' />
                 <p className='text-md font-medium'>HTML</p>
                 <input type='range' value={10} min={0} max={10} className='w-20' />
                 <div className='space-y-2'>
@@ -53,7 +55,7 @@ export default function About() {
               </li>
 
               <li className='flex justify-between mx-2 items-center'>
-                <img src={Javascript} alt='Javascript' className='w-5 h-5' />
+                <img src={Javascript} alt='Javascript' className='w-8 h-8' />
                 <p className='text-md font-medium'>Javascript</p>
                 <input type='range' value={6} min={0} max={10} className='w-20' />
                 <div className='space-y-2'>
@@ -63,7 +65,7 @@ export default function About() {
               </li>
 
               <li className='flex justify-between mx-2 items-center'>
-                <img src={Tailwind} alt='Tailwind' className='w-5 h-5' />
+                <img src={Tailwind} alt='Tailwind' className='w-8 h-8' />
                 <p className='text-md font-medium'>Tailwind CSS</p>
                 <input type='range' value={6} min={0} max={10} className='w-20' />
                 <div className='space-y-2'>
@@ -73,7 +75,7 @@ export default function About() {
               </li>
 
               <li className='flex justify-between mx-2 items-center'>
-                <img src={Code} alt='Next.Js' className='w-5 h-5' />
+                <img src={Code} alt='Next.Js' className='w-8 h-8' />
                 <p className='text-md font-medium'>Next.js</p>
                 <input type='range' value={2} min={0} max={10} className='w-20' />
                 <div className='space-y-2'>
@@ -83,7 +85,7 @@ export default function About() {
               </li>
 
               <li className='flex justify-between mx-2 items-center'>
-                <img src={Css} alt='Tailwind' className='w-5 h-5' />
+                <img src={Css} alt='Tailwind' className='w-8 h-8' />
                 <p className='text-md font-medium'>CSS Module</p>
                 <input type='range' value={4} min={0} max={10} className='w-20' />
                 <div className='space-y-2'>
@@ -95,7 +97,7 @@ export default function About() {
 
             </ul>
           </div>
-          <div className='bg-gray-500 rounded-md pt-3 space-y-2 md:w-fit md:m-3 md:rounded-lg'>
+          <div className='bg-gray-700 rounded-md pt-3 space-y-2 md:w-fit md:m-3 md:rounded-lg'>
             <h5 className='text-2xl font-black mx-2'>Work Experience</h5>
             <ul className='mx-2 pb-5'>
               <li className='py-5'>
@@ -142,6 +144,29 @@ export default function About() {
             </ul>
           </div>
         </div>
+
+
+      <h2 className='text-2xl text-center font-extrabold mt-10 mb-3'>My Core Values & Approach</h2>
+
+       <div className='space-y-5 mx-2 mb-5 md:flex md:gap-3 md:w-fit '>   
+         <div className='bg-gray-700 rounded-md pt-3 space-y-2 text-center'>
+          <img src={Bulb} alt='Ideas' className='w-8 h-8 mx-auto'/>
+          <h4 className='text-xl font-bold'>Continuous Learning</h4>
+          <p className='opacity-70 mx-2 md:pb-5'>Staying updated with the latest technologies and best practices to deliver cutting-edge solutions</p>
+          </div>
+
+          <div className='bg-gray-700 rounded-md pt-3 space-y-2 text-center'>
+          <img src={Chat} alt='Values' className='w-8 h-8 mx-auto'/>
+          <h4 className='text-xl font-bold'>User-Centric Design</h4>
+          <p className='opacity-70 mx-2'>Prioritizing the end-user experience,ensuring interfaces are intuitive, accessible and enjoyable.</p>
+          </div>
+
+          <div className='bg-gray-700 rounded-md pt-3 space-y-2 text-center'>
+          <img src={Code} alt='Ideas' className='w-8 h-8 mx-auto'/>
+          <h4 className='text-xl font-bold'>Clean Code Advocate</h4>
+          <p className='opacity-70 mx-2 md:self-start'>Writing maintainable,scalable and well-documented code that stands the test of time.</p>
+          </div>
+       </div>
 
         
       </section>
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
+import emailjs from 'emailjs-com'
+
+
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
+
+import Send from '../assets/telegram.png'
 
 export default function Contact() {
+  const [form, setForm] = useState({ name: "", email: "", message: ""});
+
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name]: e.target.value});
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    
+  
+    emailjs
+    .send(
+      "service_n8h3y07",
+      "template_fyzd738",
+      form,
+      "hhl017S8N1cMmULrn"
+    )
+
+      setForm({ name: "", email: "", message: ""});
+  }
   return (
-    <div>Contact</div>
+   <>
+   <Navbar />
+   <div className='bg-gray-800 mx-5 my-5 rounded-xl md:w-fit md:mx-auto'>
+    <form onSubmit={sendEmail} className='px-auto mx-2 py-5'>
+      <h2 className='text-xl font-bold text-center pt-2'>Get in Touch</h2>
+      <p className='mx-2 text-sm text-center pt-2'>Have a project idea, or a question? Feel free to use the contact form.</p>
+
+      <p className='font-medium pt-3'>Your Name</p>
+      <input
+      type='text'
+      name='name'
+      placeholder='Enter your name'
+      value={form.name}
+      onChange={handleChange}
+      required
+      className='bg-gray-500 rounded-md px-2 py-1 w-full'
+      />
+
+      <p className='font-medium pt-3'>Your Email</p>
+      <input 
+      type='email'
+      name='email'
+      placeholder='Enter your Email'
+      value={form.email}
+      onChange={handleChange}
+      required
+      className='bg-gray-500 rounded-md px-2 py-1 w-full'
+      />
+
+      <p className='font-medium pt-3'>Your Message</p>
+      <textarea
+      name='message'
+      placeholder='Tell me about your project or question'
+      value={form.message}
+      onChange={handleChange}
+      required
+      rows='5'
+      className='bg-gray-500 rounded-md text-sm px-2 py-1 w-full'
+       ></textarea>
+
+      
+       <button type='submit'  className='mt-3 bg-blue-700 py-1 font-semibold w-full rounded-lg'>
+        Send Message
+       </button>
+
+       
+   </form>
+   </div>
+   <Footer />
+   </>
   )
 }
